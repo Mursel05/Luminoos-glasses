e9d79fe603795079cd53739b80142beb1c02aa9a
Mursel Haxverdiyev <mursal.haqverdiyev@mail.ru>
2023-08-07 12:38:53 +0400

    navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,26 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="logo">
-        <span>LUMIN</span>
-        <img src="/images/logo/logo-light.png" alt="logo" />
-        <span>S</span>
-      </div>
+      <NavLink className="logo-link" to="/">
+        <div className="logo">
+          <span>LUMIN</span>
+          <img src="/images/logo/logo-light.png" alt="logo" />
+          <span>S</span>
+        </div>{" "}
+      </NavLink>
       <div className="links">
-        <NavLink className="link">Sunglasses</NavLink>
-        <NavLink className="link">Eyeglasses</NavLink>
-        <NavLink className="link">Game Glasses</NavLink>
-        <NavLink className="link">Service</NavLink>
+        <NavLink className="link" to="/Sunglasses">
+          Sunglasses
+        </NavLink>
+        <NavLink className="link" to="/Eyeglasses">
+          Eyeglasses
+        </NavLink>
+        <NavLink className="link" to="/Game Glasses">
+          Game Glasses
+        </NavLink>
+        <NavLink className="link" to="/Service">
+          Service
+        </NavLink>
       </div>
     </div>
   );
